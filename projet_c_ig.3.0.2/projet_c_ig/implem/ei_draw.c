9a56067aa5dac8a55f69c3974bda8de63e001847
Thomas Serafin <Thomas.Serafin@grenoble-inp.org>
2023-05-02 17:51:44 +0200

    ei_draw.c

--- projet_c_ig.3.0.2/projet_c_ig/implem/ei_draw.c
+++ projet_c_ig.3.0.2/projet_c_ig/implem/ei_draw.c
@@ -7,20 +7,24 @@ void ei_fill(ei_surface_t surface, const ei_color_t* color, const ei_rect_t* cli
     hw_surface_lock(surface); 
 
     //Returns a pointer to the address of the pixel at coordinated (0, 0) of the surface.
-    uint8_t* surface_buffer = hw_surface_get_buffer(surface); 
+    uint32_t* surface_buffer = hw_surface_get_buffer(surface); 
 
     //test if color is NULL
     if (color == NULL){
         //painted black (opaque).
 
     } else {
-        //paint with color and the drawing is restricted within the rectangle (clipper)
-        
+        //paint of the surface with color (don't take clipper into account for now)
+
+
 
     }
 
     //unlock the surface
     hw_surface_unlock(surface);
+
+    //Requests that a list of rectangular regions of the root surface be updated on screen. (NULL -> update all the screen)
+    hw_surface_update_rects(surface, NULL);
 }
 
 
