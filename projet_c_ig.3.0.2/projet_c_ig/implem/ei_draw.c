585446bd9bf4aea3e82760045de4df4d416c3f31
Thomas Serafin <Thomas.Serafin@grenoble-inp.org>
2023-05-03 10:27:30 +0200

    ei_draw

--- projet_c_ig.3.0.2/projet_c_ig/implem/ei_draw.c
+++ projet_c_ig.3.0.2/projet_c_ig/implem/ei_draw.c
@@ -26,16 +26,50 @@ void ei_fill(ei_surface_t surface, const ei_color_t* color, const ei_rect_t* cli
     int width = size.width;
     int height = size.height;
 
+    int nb_pixel = width*height;
+
+    //get order of colors in pixel
+    int ir, ig, ib, ia;
+    hw_surface_get_channel_indices(surface, &ir, &ig, &ib, &ia);
+
     //test if color is NULL
     if (color == NULL){
-        //painted black (opaque).
-        
+        //painted black (opaque)
+
+        for(int i = 0; i <= nb_pixel - 1; i++){
+            //treatment of pixel
+
+            //create vector of 4 byte (4 octet)
+            uint32_t pixel_value;
+            //déclare pointeur vers pixel_value
+            uint8_t* channel_ptr = (uint8_t*) &pixel_value;
+
+            channel_ptr[ir] = 0x00;
+            channel_ptr[ig] = 0x00;
+            channel_ptr[ib] = 0x00;
+            channel_ptr[ia] = 0xff;
+
+            *surface_buffer++ = pixel_value; //<=> *surface_buffer=pixel_value; surface_buffer++
+        }
 
     } else {
         //paint of the surface with color (don't take clipper into account for now)
 
+        for(int i = 0; i <= nb_pixel - 1; i++){
+            //treatment of pixel
 
+            //create vector of 4 byte (4 octet)
+            uint32_t pixel_value;
+            //déclare pointeur vers pixel_value
+            uint8_t* channel_ptr = (uint8_t*) &pixel_value;
 
+            channel_ptr[ir] = color->red;
+            channel_ptr[ig] = color->green;
+            channel_ptr[ib] = color->blue;
+            channel_ptr[ia] = color->alpha;
+
+            *surface_buffer++ = pixel_value; //<=> *surface_buffer=pixel_value; surface_buffer++
+        }
     }
 
     //unlock the surface
@@ -44,6 +78,8 @@ void ei_fill(ei_surface_t surface, const ei_color_t* color, const ei_rect_t* cli
     //Requests that a list of rectangular regions of the root surface be updated on screen. (NULL -> update all the screen)
     hw_surface_update_rects(surface, NULL);
 }
+
+
 
 
 
