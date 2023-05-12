0a9f41190fafe25fea8351ebeac314b5d4277939
Thomas Serafin <Thomas.Serafin@grenoble-inp.org>
2023-05-12 14:40:25 +0200

    button

--- projet_c_ig/implem/ei_widget.c
+++ projet_c_ig/implem/ei_widget.c
@@ -6,11 +6,13 @@
 #include "ei_widget_configure.h"
 #include "ei_placer.h"
 
+
+
 ei_widget_t		ei_widget_create		(ei_const_string_t	class_name,
 							    ei_widget_t		parent,
 							    ei_user_param_t	user_data,
 							    ei_widget_destructor_t destructor) {
-
+	
 }
 
 void			ei_frame_configure		(ei_widget_t		widget,
--- projet_c_ig/implem/ei_widgetclass.c
+++ projet_c_ig/implem/ei_widgetclass.c
@@ -3,6 +3,17 @@
 //
 #include "ei_widgetclass.h"
 
+/*Chained list of class*/
+ei_widgetclass_t * list_class = NULL;
+
 void ei_widgetclass_register (ei_widgetclass_t* widgetclass) {
-	
+	if(list_class == NULL){
+		*list_class = *widgetclass;
+	} else {
+		ei_widgetclass_t * current = list_class;
+		while (current->next != NULL) {
+			current = current->next;
+		}
+		current->next = widgetclass;
+	}
 }
