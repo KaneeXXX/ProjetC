d3e0b4f95dd220efe7cdc74f946dad3b9e09a22c
Jean-Minh Grégoire <Jean-Minh.Gregoire@grenoble-inp.org>
2023-05-16 13:29:51 +0200

    minimal réinitialisé

--- projet_c_ig/tests/minimal.c
+++ projet_c_ig/tests/minimal.c
@@ -4,8 +4,6 @@
 #include "ei_event.h"
 #include "ei_utils.h"
 #include "hw_interface.h"
-#include "ei_types.h"
-#include "ei_draw.h"
 
 int main(int argc, char* argv[])
 {
@@ -39,54 +37,6 @@ int main(int argc, char* argv[])
 
 	for (i = 0; i < (main_window_size.width * (main_window_size.height/2)); i++)
 		*pixel_ptr++ = *((uint32_t*)red);
-
-	//FIGURE DU SUJET POUR DRAW_POLYGON
-	ei_point_t  point_array[] = {{20, 30}, {90, 10},{140, 10},{140, 50}, {120, 100}, {70, 70}, {20, 90}};
-	size_t length=7;
-	ei_color_t color = {0, 255, 255, 0};
-//	ei_rect_t clipper_poly		= ei_rect(ei_point(0, 0), ei_size(100, 50)); //TEST AVEC CLIPPER
-//	ei_rect_t* clipper_poly_ptr		= &clipper_poly;
-	ei_draw_polygon (main_window, point_array, length, color, NULL);
-
-	/*//DRAW RECT
-	ei_point_t  point_array[] = {{200, 200}, {100,200}, {100, 100}, {200, 100}};
-	size_t length=4;
-	ei_color_t color = {0, 255, 255, 0};
-	ei_draw_polyline(main_window, point_array, length, color, NULL);*/
-
-	ei_color_t noir = {0, 0, 0, 0xff};
-	pixel_ptr = (uint32_t*)hw_surface_get_buffer(main_window);
-	for(int i=0; i < (int) length; i++) {
-		u_int32_t *point = pixel_ptr + main_window_size.width * point_array[i].y + point_array[i].x;
-		//Get order of colors in pixel because it's not always the same
-
-		uint32_t pixel_value;
-		uint8_t* channel_ptr = (uint8_t*) &pixel_value; //ptr to pixel_value
-		channel_ptr[ir] = noir.red;
-		channel_ptr[ig] = noir.green;
-		channel_ptr[ib] = noir.blue;
-
-		*point = pixel_value;
-	}
-
-	/*//TEST ARC TRIGO
-	ei_point_t center = {10,10};
-	int rayon=10;
-	arc(center, rayon, 0, 90);
-	printf("------------------------");
-	arc(center, rayon, 90, 180);*/
-
-	//TEST DE DRAW_BUTTON*/
-	ei_size_t size={150, 100};
-	ei_point_t topleft = {300, 200};
-	ei_rect_t rectangle = {topleft, size};
-	draw_button(main_window, rectangle, 16, ei_relief_raised);
-
-	//TEST DE DRAW_TEXT
-	ei_point_t where = {10, 10};
-	ei_const_string_t text = "bonjour";
-	ei_font_t super_font = hw_text_font_create(ei_default_font_filename, ei_style_normal, 5);
-	ei_draw_text(main_window, &where, text, super_font, noir, NULL);
 
 	// unlock, update screen.
 	hw_surface_unlock(main_window);
